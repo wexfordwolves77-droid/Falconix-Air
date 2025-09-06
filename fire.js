0d003c34c71e2f185eda2b9e393e469478856dce
LouieAir <wexfordwolves77@gmail.com>
2025-09-06 07:59:19 -0400

    Create fire.js

---
+++ fire.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD1qFKFejjvNOpd2N8a6a16rIzxx3-zD3U",
+  authDomain: "falcinix-airways.firebaseapp.com",
+  projectId: "falcinix-airways",
+  storageBucket: "falcinix-airways.firebasestorage.app",
+  messagingSenderId: "533257481224",
+  appId: "1:533257481224:web:c0cb8c491be9b9394cf5bd"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
